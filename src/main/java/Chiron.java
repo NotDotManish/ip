c739a3e9fc827641a25f7a2c9943630584e9fa2e
NotDotManish <Mans7de@gmail.com>
2026-01-31 17:33:53 +0800

    Level-0: Rename Duke to Chiron, add greeting and exit

---
+++ src/main/java/Chiron.java
@@ -0,0 +1,16 @@
+public class Chiron {
+
+    private static final String LINE =
+            "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Chiron!");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
