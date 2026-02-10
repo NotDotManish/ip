d7742f4f7d54df4b7aa59d9cb5b4114ec0111f4c
NotDotManish <Mans7de@gmail.com>
2026-02-10 18:39:17 +0800

    A-JUnit: Add Parser and TaskList tests

---
+++ src/test/java/chiron/TaskListTest.java
@@ -0,0 +1,41 @@
+package chiron;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void add_task_success() {
+        // Verify that adding a task increases the list size
+        TaskList tasks = new TaskList();
+        assertEquals(0, tasks.size());
+
+        tasks.add(new Todo("read book"));
+        assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void delete_task_success() {
+        // Verify that removing a task decreases list size and returns the correct task
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("task 1"));
+        tasks.add(new Todo("task 2"));
+        assertEquals(2, tasks.size());
+
+        Task removed = tasks.remove(0);
+        assertEquals("task 1", removed.getDescription());
+        assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void get_task_success() {
+        // Verify retrieving a task by index
+        TaskList tasks = new TaskList();
+        Task t = new Todo("test task");
+        tasks.add(t);
+
+        assertEquals(t, tasks.get(0));
+    }
+}
