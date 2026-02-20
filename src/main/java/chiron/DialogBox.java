d71c521dbcc5e64f3fce8754ee444c4ff31a8576
NotDotManish <Mans7de@gmail.com>
2026-02-21 03:24:13 +0800

    A-BetterGui & A-Personality: Improve UI aesthetics
    Update MainWindow and DialogBox layouts to support resizing, anchoring, and asymmetric chat bubbles via main.css. Add custom profile pictures and background image for Chiron's personality.

--- src/main/java/chiron/DialogBox.java
+++ src/main/java/chiron/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -38,6 +39,10 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+
+        Circle clip = new Circle(displayPicture.getFitWidth() / 2, displayPicture.getFitHeight() / 2,
+                displayPicture.getFitWidth() / 2);
+        displayPicture.setClip(clip);
     }
 
     /**
@@ -52,12 +57,15 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        var db = new DialogBox(text, img);
+        db.getStyleClass().add("user-dialog-box");
+        return db;
     }
 
     public static DialogBox getChironDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.getStyleClass().add("chiron-dialog-box");
         return db;
     }
 }
