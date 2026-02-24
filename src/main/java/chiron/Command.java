2131bd8f581b721d26bb1612acb14c20b92e379b
NotDotManish <Mans7de@gmail.com>
2026-02-24 17:29:48 +0800

    Improve JavaDoc wording for return values
    Standardize the capitalization of boolean literals in the JavaDoc comments to conform directly to standard conventions.

--- src/main/java/chiron/Command.java
+++ src/main/java/chiron/Command.java
@@ -11,7 +11,7 @@ public abstract class Command {
      * @param tasks   The list of tasks.
      * @param ui      The user interface.
      * @param storage The storage handler.
-     * @return True if the application should exit, false otherwise.
+     * @return true if the application should exit, false otherwise.
      * @throws ChironException If an error occurs during execution.
      */
     public abstract boolean execute(TaskList tasks, Ui ui, Storage storage) throws ChironException;
